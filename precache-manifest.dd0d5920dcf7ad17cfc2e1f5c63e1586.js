a4e9ddccbea9a5985161e74ea957aa0513c331a4
fgalletti93 <fgalletti93@yahoo.com>
2022-01-16 09:30:56 -0300

    Updates

---
+++ precache-manifest.dd0d5920dcf7ad17cfc2e1f5c63e1586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578cb899cc9f7bfa1ae0bb48b8bd27bf",
+    "url": "/react-website-yt/index.html"
+  },
+  {
+    "revision": "7fa7646d5415b5430e5f",
+    "url": "/react-website-yt/static/css/main.37dd3d3d.chunk.css"
+  },
+  {
+    "revision": "d94d8d5a8c619cb70c16",
+    "url": "/react-website-yt/static/js/2.4a5c4c96.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/react-website-yt/static/js/2.4a5c4c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa7646d5415b5430e5f",
+    "url": "/react-website-yt/static/js/main.387747bb.chunk.js"
+  },
+  {
+    "revision": "4bbc1f234db530386ee5",
+    "url": "/react-website-yt/static/js/runtime-main.5c866399.js"
+  }
+]);
